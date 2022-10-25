2dc8a8a571de5ab9d385d21e2be9da9e861e3f9c
unknown <faisaljfcl@gmail.com>
2022-10-25 16:00:01 +0600

    Resolved the CORS issue

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-  res.send(data);
+  res.send({ error: false, working: true, data });
+});
+
+app.get("/:postId", (req, res) => {
+  const { postId } = req.params;
+  const post = data.find((item) => item.id == postId);
+  res.send(post);
 });
 
 app.listen(PORT, () => console.log(`SERVER is listening to port: ${PORT}`));
